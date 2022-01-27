072396712517568c8a833c099fd82e225298dce3
imanulik <i.manulik@godeltech.com>
2022-01-27 18:59:04 +0300

    initial commit

---
+++ build/precache-manifest.05b0890dbae672401876b6bc2321f854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1227d42388488830aae565b6f99bab55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405168d5adf7eb3d2ffb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e88cf915786643f1267f",
+    "url": "/static/js/2.56686953.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.56686953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405168d5adf7eb3d2ffb",
+    "url": "/static/js/main.3eed35d2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
